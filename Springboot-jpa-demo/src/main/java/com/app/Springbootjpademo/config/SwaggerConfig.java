1414623fc7cac87d9cdd6aced1656465b4ecf4e6
SWAPNIL <swapnil14392@gmail.com>
2022-07-19 01:22:33 +0530

    [new:] fetch swagger details using OpenApi bean: enhancement

--- Springboot-jpa-demo/src/main/java/com/app/Springbootjpademo/config/SwaggerConfig.java
+++ Springboot-jpa-demo/src/main/java/com/app/Springbootjpademo/config/SwaggerConfig.java
@@ -1,12 +1,24 @@
 package com.app.Springbootjpademo.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 
 @Component
-@OpenAPIDefinition(info = @Info(title = "Merchant API",version = "1.0",description = "Merchant Api UI"))
+//@OpenAPIDefinition(info = @Info(title = "Merchant API",version = "1.0",description = "Merchant Api UI"))
 public class SwaggerConfig {
-
+	
+	@Bean
+	public OpenAPI customOpenAPI(@Value("${application-description}") String appDesciption,
+			@Value("${application-version}") String appVersion) {
+		return new OpenAPI().info(
+				new Info().title("sample Merchant API").
+				version(appVersion).description(appDesciption)
+				.termsOfService("http://swagger.io/terms/")
+				.license(new License().name("Apache 2.0").url("http://springdoc.org")));
+	}
 }
